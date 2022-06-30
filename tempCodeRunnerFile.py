96c313b950a874943c5e0ce6672bca6e54c44edc
Shaxzodb <shaxzodbmaster@gmail.com>
2022-07-01 00:10:55 +0500

    bot

--- tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-if message.text.lower().startswith('https://www.tiktok.com/') or message.text.lower().startswith('https://vt.tiktok.com/') or message.text.lower().startswith('https://tiktok.com/') \
-                            or message.text.lower().startswith('https://vm.tiktok.com/'):
-                            await tiktok(message,os.getenv('THOST'),os.getenv('KEY'),os.getenv('TURL'))
-                            break
